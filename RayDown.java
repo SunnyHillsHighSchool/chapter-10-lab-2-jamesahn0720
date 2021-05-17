b64db8a7d535e36c84cb52a11d2b4baed7831335
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 23:12:03 -0700

    Update RayDown.java

--- RayDown.java
+++ RayDown.java
@@ -9,14 +9,15 @@ public class RayDown
  //are in decreasing order [31,12,6,2,1]
  public static boolean go(int[] numArray)
  {
-   for(int i=0 ; i<numArray.length-1 ; i++) 
-   {
-    if(numArray[i] <= numArray[i+1])
-    {
-                         
-        return false;
-    }
-  return true;
+    Scanner s = new Scanner(System.in);
+   System.out.println("Enter the size of array must be greater than 0 :");
+   int n = s.nextInt();
+   int arr[] = new int[n];
+     
+   for(int i=0;i<n;i++) {
+       arr[i] = s.nextInt();
    }
- } 
+     
+   System.out.println(DownRunner.isDecreasing(arr));
+   }
 }
